c41e10d9c81a76e17451a93f1c104e7bdaebe53e
felix <felix@DESKTOP-SSJR4EI>
2022-09-08 18:17:57 +0200

    Add project files.

---
+++ Kap 1-3 uppgift 6/Kap_1_3_uppgift_6.py
@@ -0,0 +1,10 @@
+tal1=int(input("Tal 1.= "))
+tal2=int(input("Tal 2. ="))
+tal3=int(input("Tal 3. ="))
+summa=tal1+tal2+tal3
+
+print("    ",tal1)
+print("     ",tal2)
+print("+  ",tal3)
+print("=======")
+print("   ",summa)
